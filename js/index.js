a266854e81d054c5d7c10f05bda5cacf1910c166
Colette-Liu <colette.liu@gmail.com>
2021-07-13 12:36:18 +1000

    update js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // window.addEventListener("keydown", function (e) {
 //   const audio = document.querySelector(`audio[data-key="${e.key}]`);
 //   if (!audio) return;
+//   audio.currentTime = 0;
 //   audio.play();
+//   key.classList.add("playing");
 // });
 
 function removeTransition(e) {
